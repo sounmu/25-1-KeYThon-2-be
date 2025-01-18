7345cb5d1d40c66c1e360754474beadbaf5a1132
sounmu <hyginus011111@gmail.com>
2025-01-18 23:09:56 +0900

    feat: 설문 점수 계산 로직 수정 및 비율 계산 추가

--- src/domain/service/survey_service.py
+++ src/domain/service/survey_service.py
@@ -7,16 +7,22 @@ async def service_create_survey(
     data: RouteReqPostSurvey
 ) -> RouteResPostSurvey:
     try:
-        score = 0
-        for i in range(20):
-            answer = data.answers[i]
-            # 새로운 로직 필요
-            score += answer
+        weights = [[0.549, 0.144, 0.307], [0.601, 0.200, 0.199], [0.300, 0.400, 0.300]]
+        left, center, right = 0, 0, 0
+        for answer, weight in zip(data.answers, weights):
+            left += answer * weight[0]
+            center += answer * weight[1]
+            right += answer * weight[2]
+        total = left + center + right
+
+        left_percentage = (left/total) * 100
+        center_percentage = (center/total) * 100
+        right_percentage = (right/total) * 100
 
         res = DomainResSurveyResult(
-            progressive=score,
-            moderate=score,
-            conservative=score,
+            progressive=left_percentage,
+            moderate=center_percentage,
+            conservative=right_percentage,
         )
 
         response = RouteResPostSurvey(
